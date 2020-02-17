0422840542ff54be7e66f31dfada5e20282a0d4a
ndvo2710 <kevinvo2710@gmail.com>
2020-02-17 00:27:13 +0000

    Set up template partials and template views

--- app.js
+++ app.js
@@ -1,5 +1,39 @@
-function main(){
-	console.log("Hello goorm!");
-}
+const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+const app = express()
+const port = process.env.PORT || 3000
 
-main();
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, './frontend')
+console.log(publicDirectoryPath)
+
+const viewsPath = path.join(__dirname, './frontend/templates/views')
+const partialsPath = path.join(__dirname, './frontend/templates/partials')
+
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Kevin Vo'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Kevin Vo'
+    })
+})
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
